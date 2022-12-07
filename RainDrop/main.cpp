9cdd16aa210d266e176a70653abbce09e9060884
Insori <s2110@e-mirim.hs.kr>
2022-12-07 21:30:23 +0900

    기획 수정, 불 떨어지기

--- RainDrop/main.cpp
+++ RainDrop/main.cpp
@@ -2,37 +2,48 @@
 #include <iostream>
 #include <vector>
 
+#define MAX_FIRE 5
+
 using namespace std;
 using namespace sf;
 
-void main() {
-	bool game_over = false; //게임 오버 여부
-	bool game_clear = false;    //게임 클리어 여부
-	int score = 0;
-	//TODO: 타이머 구현
+void main() 
+{
+	//타이머
+	Clock clock;
+	float interval = 0;
 
-	srand(time(NULL));
+	bool game_over = false; //게임 오버 여부
+
+	srand((unsigned int)time(NULL));
 
 	RenderWindow app(VideoMode(700, 700), "RainDrop");
 	app.setFramerateLimit(60);	//프레임 비율 설정
 
-	Texture d1, d2, d3, d4;
+	Texture d1, d2, d3;
 	d1.loadFromFile("images/background.png");
 	d2.loadFromFile("images/1.png");
-	d3.loadFromFile("images/sun.png");
-	d4.loadFromFile("images/fire.png");
+	d3.loadFromFile("images/fire.png");
 
 	Sprite background(d1), player(d2);
 	player.setPosition(325, 605);
 
-	Sprite sun(d3);
-	vector<Sprite> v_sun;
-
-	Sprite fire(d4);
+	Sprite fire(d3);
 	vector<Sprite> v_fire;
 
+	for (int i = 0; i < MAX_FIRE; i++) 
+	{
+		fire.setPosition(rand() % 650, -50);
+		v_fire.push_back(fire);
+	}
 
-	while (app.isOpen()) {
+	while (app.isOpen()) 
+	{
+
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		interval += time;
+
 		Event event;
 
 		while (app.pollEvent(event))
@@ -50,53 +61,48 @@ void main() {
 			}
 
 		}
-
 		//좌우로 움직이기
 		if (Keyboard::isKeyPressed(Keyboard::Right))
 			player.move(7, 0);
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 			player.move(-7, 0);
 
-		//햇빛 떨어지기
-		v_sun.push_back(sun);
+		app.clear();
 
-		for (int i = 0; i < v_sun.size(); i++)
+		//불 떨어지기
+		if ((int)interval % 1 == 0)
 		{
-			sun.setPosition(rand() % 650, -50);
-			v_sun[i].move(0, 5);
-			//화면 밖으로 나가면 지워주기
-			if (v_sun[i].getPosition().y > 700) v_sun.erase(v_sun.begin() + i);
-			//햇빛 맞으면 점수 올라가기
-			if (player.getGlobalBounds().intersects(v_sun[i].getGlobalBounds()))
+			vector<Sprite>::iterator iter;
+			for (iter = v_fire.begin(); iter != v_fire.end(); iter++)
 			{
-				score += 10;
-				v_sun.erase(v_sun.begin() + i);
+				(*iter).move(0, rand() % (10 - 7 + 1) + 7);
+
+				Vector2f pos = (*iter).getPosition();
+				if (pos.y > 700) (*iter).setPosition(rand() % 700, 800);
 			}
 		}
 
-		//불 떨어지기
-		v_fire.push_back(fire);
-
+		//충돌
+		vector<Sprite>::iterator iter;
 		for (int i = 0; i < v_fire.size(); i++)
 		{
-			fire.setPosition(rand() % 650, -50);
-			v_fire[i].move(0, 5);
-			if (v_fire[i].getPosition().y > 700) v_fire.erase(v_fire.begin() + i);
-			//불 맞으면 게임 오버
 			if (player.getGlobalBounds().intersects(v_fire[i].getGlobalBounds()))
 			{
 				v_fire.erase(v_fire.begin() + i);
 				game_over = true;
 			}
+
 		}
+
+		
 		//그리기
 		app.clear();
 		app.draw(background);
 		app.draw(player);
-		for (int i = 0; i < v_sun.size(); i++)
-			app.draw(v_sun[i]);
-		for (int i = 0; i < v_fire.size(); i++)
-			app.draw(v_fire[i]);
+		for (iter = v_fire.begin(); iter != v_fire.end(); iter++)
+		{
+			app.draw(*iter);
+		}
 
 		app.display();
 
